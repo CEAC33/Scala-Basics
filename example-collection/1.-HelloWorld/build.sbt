4e938f89a812ac98c826a2350d523f53105ef663
Carlos Avila <cavila@vyze.com>
2017-12-28 11:13:14 -0600

    1-HelloWorld Added

---
+++ example-collection/1.-HelloWorld/build.sbt
@@ -0,0 +1,6 @@
+name := "1.-HelloWorld"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+        
---
+++ example-collection/1.-HelloWorld/src/main/scala/Demo0.scala
@@ -0,0 +1,18 @@
+package Demo0
+
+// 1.- public (object, method)
+// 2.- object =~ static
+// 3.- def
+// 4.- invert type information
+// 5.- arrays + generics in Scala
+// 6.- String, Int, Unit in Scala
+// 7.- implicit imports
+
+/*public*/ object Demo0
+{
+
+  /*public*/ def main(args: Array[String])
+  {
+    println("\n\n\nHello World!\n\n\n")
+  }
+}
